181cca21c700447592e025b168fa98d4c7650179
Harsh Raj <hraj26058@gmail.com>
2021-06-07 15:59:34 +0530

    synchronous Ball Movement

--- sketch.js
+++ sketch.js
@@ -1,29 +1,44 @@
-var ball;
+var ball1;
 
 function setup(){
+    database= firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    ball1 = createSprite(250,250,10,10);
+    var ball1Position= database.ref('ball/position');
+     ball1Position.on("value",readPosition,showError)
+    ball1.shapeColor = "red";
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    // ball1.x = ball1.x + x;
+    // ball1.y = ball1.y + y;
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+}
+function readPosition(data){
+    position=data.val();
+    ball1.x=position.x;
+    ball1.y=position.y;
+}
+function showError(){
+    console.log("Error in writing to the database");
 }
